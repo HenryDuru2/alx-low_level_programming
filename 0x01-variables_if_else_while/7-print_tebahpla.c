40322e856ec833e8bca1efbe1cd1536b3f8d8378
Ghostprime1 <henryduru2@gmail.com>
2023-02-19 08:24:32 -0800

    tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry ponint
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
