1307fe2d2333bf50d9d4ff91e95acd8c8ed30497
Ghostprime1 <henryduru2@gmail.com>
2023-02-19 08:03:04 -0800

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry ponint
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	if (ch != 'e' && ch != 'q')
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
