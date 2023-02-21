9d945466500b6b4b4b4c658d29f790a6f5bb78df
Ghostprime1 <henryduru2@gmail.com>
2023-02-21 10:44:15 -0800

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point
+ *
+ * Return Always 0 (Success)
+*/
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	return (0);
+}
