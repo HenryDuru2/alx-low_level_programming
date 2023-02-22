ff938b073666a7441a31674af0717f9c3118ae87
Ghostprime1 <henryduru2@gmail.com>
2023-02-22 14:29:10 -0800

    Last Digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - Last digits
+ *
+ * @n: character
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	int lastdigit;
+
+	lastdigit = n % 10;
+	if (lastdigit > 0)
+	{
+		lastdigit = lastdigit * -1;
+	}
+	_putchar(lastdigit + '0');
+	return (lastdigit);
+}
