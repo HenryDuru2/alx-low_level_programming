35ea5b86b4b65991ec29972c10bad144f778b044
Ghostprime1 <henryduru2@gmail.com>
2023-02-24 07:25:16 -0800

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - prints line
+ *
+ * @n: integer
+ *
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int k;
+
+	for (k = 0; k < n; k++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
