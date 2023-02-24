e83f0faf9ad6d7d59876db605ec8fc91652cb3d9
Ghostprime1 <henryduru2@gmail.com>
2023-02-24 04:16:54 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *
+ * Description: Print numbers
+ *
+ * Return: ALWAYS 0
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		_putchar(ch);
+		_putchar('\n');
+	}
+
+}
