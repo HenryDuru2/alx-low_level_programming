6f262eec91f823f65a2dbe68eae7e1c68c236c49
Ghostprime1 <henryduru2@gmail.com>
2023-02-27 06:28:47 -0800

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - changes number to pointer
+ *
+ * @n:integer or character
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
