6b40429be51b8b4fa854fd190e647cfd26ccf0c2
Ghostprime1 <henryduru2@gmail.com>
2023-02-27 08:42:26 -0800

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - length of a string
+ *
+ * @s: integer
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (0);
+}
