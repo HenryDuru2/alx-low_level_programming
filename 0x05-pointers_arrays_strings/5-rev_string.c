507574ddd14b657d00d762ea65d8b871eb9aefbe
Ghostprime1 <henryduru2@gmail.com>
2023-02-27 14:22:38 -0800

    string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses string
+ *
+ * @s: integer
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int i, temp;
+	int len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+	_putchar(s[i]);
+}
