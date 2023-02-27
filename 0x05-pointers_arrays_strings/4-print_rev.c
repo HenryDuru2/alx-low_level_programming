4163120d7b90ef5a97f718f4649766c4f9c35c01
Ghostprime1 <henryduru2@gmail.com>
2023-02-27 12:14:07 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - Reverses a string
+ *
+ * @s: integer
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+			i++;
+		}
+		for (i--; i >= 0; i--)
+		{
+		_putchar(s[i]);
+		}
+	}
+}
