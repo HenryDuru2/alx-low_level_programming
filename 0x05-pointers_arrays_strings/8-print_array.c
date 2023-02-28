7bc825498e900535f168e70ca3148c76c55d73ef
Ghostprime1 <henryduru2@gmail.com>
2023-02-28 09:24:41 -0800

    arrar

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints arrays
+ * @a: integer
+ * @n: integer
+ * Return: Always 0
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+}
