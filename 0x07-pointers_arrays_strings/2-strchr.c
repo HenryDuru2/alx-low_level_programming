32feb161e64375bc7b4c557941168e8006f13a6e
Ghostprime1 <henryduru2@gmail.com>
2023-03-06 12:04:46 -0800

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - locates character in a string
+ *
+ * @s: string
+ * @c: characters
+ * Return: s or Null if c not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}
