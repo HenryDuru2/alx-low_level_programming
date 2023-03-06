22caf07db3882edd5ae43e8df9c53029ea1a2cec
Ghostprime1 <henryduru2@gmail.com>
2023-03-06 11:56:00 -0800

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: Destination
+ * @src: Source
+ * @n: integer
+ *
+ * Return: Destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
+}
